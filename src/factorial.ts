737b959ab59d9c2799846bcff8c90568d247e4bc
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-27 02:23:12 +0600

    solved factorial problem

---
+++ src/factorial.ts
@@ -0,0 +1,15 @@
+const factorial = (number: number): number | never => {
+	if (number <= 0) {
+		throw new Error("Operation Not Allowed!")
+	}
+
+	let result = 1;
+
+	for (let i = number; i >= 1; i--) {
+		result *= i;
+	}
+
+	return result;
+};
+
+export default factorial(5); // 120
