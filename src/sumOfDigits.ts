c81701ad12a2332d3074cb602f7c3da1187ae8c4
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-27 01:45:56 +0600

    solved sumOfDigits problem

---
+++ src/sumOfDigits.ts
@@ -0,0 +1,17 @@
+const sumOfDigits = (number: number): number => {
+	const strNumArray = number.toString().split("");
+
+	// using reduce method
+	// return strNumArray.reduce((acc, val) => acc + Number(val), 0);
+
+	// using loop
+	let sum = 0;
+
+	for (const num of strNumArray) {
+		sum += Number(num);
+	}
+
+	return sum;
+};
+
+export default sumOfDigits(567);
