ca2f2300c0986e1cbd7b6c68b5890253638ace12
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-30 23:53:25 +0600

    update(split): updated logic in splitString; used for loop instead of while

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,36 +3,30 @@
  * @param num A numeric expression
  */
 export function floorNumber(num: number): number {
-	return Number(splitString(num?.toString(), '.')?.[0]);
+	return Number(splitString(`${num}`, '.')?.[0]);
 }
 
 /**
  * Split a string into substrings using the specified separator (single character) and return them as an array.
  * @param str A string to split.
- * @param splitBy A string that identifies a character to use in separating the string. If omitted, a single-element array containing the entire string is returned.
+ * @param splitBy - The character to split by (must be a single character).
  */
 export function splitString(str: string, splitBy: string): string[] {
-	const length = str.length;
-
 	const result: string[] = [];
+	let part = '';
 
-	let i = 0,
-		part = '';
+	for (let i = 0; i < str.length; i++) {
+		const char = str[i];
 
-	while (i <= length - 1) {
-		if (str[i] !== splitBy) {
-			part += str[i];
-		}
-
-		if (str[i] === splitBy) {
+		if (char === splitBy) {
 			result.push(part);
 			part = '';
-		} else if (str[length - 1] === str[i]) {
-			result.push(part);
+		} else {
+			part += char;
 		}
-
-		i++;
 	}
+
+	result.push(part);
 
 	return result;
 }
